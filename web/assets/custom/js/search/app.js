758cf6c55ddc12e26e78661f0d955098405ffebc
Greg Doak <grdoak@Gregs-iMac.local>
2016-03-01 08:25:48 +0000

    Changed results function

--- web/assets/custom/js/search/app.js
+++ web/assets/custom/js/search/app.js
@@ -19,7 +19,7 @@ app.controller('resultsCtrl', ['$http', '$rootScope', '$scope', function ($http,
         method: 'GET',
         url: 'results.json',
         params: {
-            start: -25,
+            start: 0,
             limit: 25,
             q: $scope.q,
             facet: true
@@ -44,8 +44,8 @@ app.controller('resultsCtrl', ['$http', '$rootScope', '$scope', function ($http,
     });
 
     function getResults(request) {
-        request.params.start = request.params.start + request.params.limit;
         $http(request).then(function (response) {
+            request.params.start = request.params.start + request.params.limit;
             var data = response.data;
             if (data.count > 0) {
                 if (request.params.start === 0) {
